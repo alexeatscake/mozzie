6e3949e9b034d8f0e67ce3ded9899c807bb63a7d
Alex Hickey <alexander.john.hickey@gmail.com>
2025-06-10 15:25:45 +0100

    :scroll: Can run with params

---
+++ py_script/run_one_set.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+
+from mozzie.generate import run_custom
+
+
+def main(params_path: str):
+    main_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+
+    script_path = os.path.join(main_dir, "GeneralMetapop/build/gdsimsapp")
+    working_dir = os.path.join(main_dir, "data")
+    params_path = os.path.join(main_dir, params_path)
+    if not os.path.exists(script_path):
+        msg = f"GDSiMS params not found at {params_path}"
+        raise FileNotFoundError(msg)
+
+    output = run_custom(
+        script_path=script_path,
+        working_dir=working_dir,
+        params_path=params_path,
+    )
+
+    print("Output from GDSiMS script:")
+    print(output)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run GDSiMS with default parameters.")
+    parser.add_argument(
+        "params_path",
+        type=str,
+        help="Relative path to the GDSiMS script.",
+    )
+    main(parser.parse_args().params_path)
--- src/mozzie/generate.py
+++ src/mozzie/generate.py
@@ -46,3 +46,48 @@ def run_default(script_path: str, working_dir: str) -> str:
 
     stdout, _ = process.communicate()
     return stdout.decode()
+
+
+def run_custom(script_path: str, working_dir: str, params_path: str) -> str:
+    """
+    Run the GDSiMS script with custom parameters.
+
+    Args:
+        script_path (str): Path to the GDSiMS script.
+        working_dir (str): Directory where the script should be run.
+        params_path (str): Path to the file containing custom parameters.
+
+    Returns:
+        str: Output from the GDSiMS script.
+    """
+    print(f"Running script: {script_path}")
+
+    process = subprocess.Popen(
+        [script_path],
+        cwd=working_dir,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    if process.stdin is None:
+        msg = "Failed to open stdin for the process."
+        raise RuntimeError(msg)
+
+    time.sleep(0.5)
+    process.stdin.write(b"100\n")
+    process.stdin.flush()
+    print("Selecting custom parameters")
+
+    time.sleep(0.5)
+    process.stdin.write(f"{params_path}\n".encode())
+    process.stdin.flush()
+    print("providing parameters file")
+
+    time.sleep(0.5)
+    process.stdin.write(b"y\n")
+    process.stdin.flush()
+    print("Starting Process")
+
+    stdout, _ = process.communicate()
+    return stdout.decode()
