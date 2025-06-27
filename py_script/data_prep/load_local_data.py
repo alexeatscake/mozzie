5281b2d6330542d4e908598f54ca190fd219fbca
Alex Hickey <alexander.john.hickey@gmail.com>
2025-06-27 16:41:27 +0100

    :house_with_garden: Local data load script

---
+++ py_script/data_prep/load_local_data.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+import argparse
+import os
+
+import pandas as pd
+
+from mozzie.data_prep import (
+    contruct_local_x_and_y,
+    load_local_values,
+    load_samples_values,
+    load_test_train,
+)
+
+
+def main(config_path: str):
+    train_config, test_config = load_test_train(config_path)
+
+    sample_values_train = load_samples_values(
+        os.path.dirname(config_path), train_config
+    )
+    local_data_train = load_local_values(os.path.dirname(config_path), train_config)
+    X_train, y_train = contruct_local_x_and_y(local_data_train, sample_values_train)
+
+    print("X train shape:", X_train.shape)
+    print("y train shape:", y_train.shape)
+
+    sample_values_test = load_samples_values(os.path.dirname(config_path), test_config)
+    local_data_test = load_local_values(os.path.dirname(config_path), test_config)
+    X_test, y_test = contruct_local_x_and_y(local_data_test, sample_values_test)
+    print("X test shape:", X_test.shape)
+    print("y test shape:", y_test.shape)
+
+    processed_data_dir = os.path.join(os.path.dirname(config_path), "processed_local")
+    os.makedirs(processed_data_dir, exist_ok=True)
+    pd.DataFrame(X_train).to_csv(
+        os.path.join(processed_data_dir, "X_train.csv"), index=False
+    )
+    pd.DataFrame(y_train).to_csv(
+        os.path.join(processed_data_dir, "y_train.csv"), index=False
+    )
+    pd.DataFrame(X_test).to_csv(
+        os.path.join(processed_data_dir, "X_test.csv"), index=False
+    )
+    pd.DataFrame(y_test).to_csv(
+        os.path.join(processed_data_dir, "y_test.csv"), index=False
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Load data form GDSiMS experiments.")
+    parser.add_argument(
+        "config_path",
+        type=str,
+        help="Relative path to the GDSiMS config file.",
+    )
+    args = parser.parse_args()
+    main_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+    config_path = os.path.join(main_dir, args.config_path)
+
+    main(args.config_path)
