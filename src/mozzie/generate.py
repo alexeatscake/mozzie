75a6383d38cff001a3a228fdfb5054b315c82bd3
Alex Hickey <alexander.john.hickey@gmail.com>
2025-08-08 14:41:42 +0100

    :globe_with_meridians: Added running with coordinates

--- src/mozzie/generate.py
+++ src/mozzie/generate.py
@@ -139,3 +139,76 @@ def run_custom(
 
     stdout, _ = process.communicate()
     return stdout.decode()
+
+
+def run_with_coords(
+    script_path: str | Path,
+    working_dir: str | Path,
+    params_path: str | Path,
+    coords_path: str | Path,
+) -> str:
+    """
+    Run the GDSiMS script with custom parameters.
+
+    Args:
+        script_path (str): Path to the GDSiMS script.
+        working_dir (str): Directory where the script should be run.
+        params_path (str): Path to the file containing custom parameters.
+        coords_path (str): Path to the file containing coordinates.
+
+    Returns:
+        str: Output from the GDSiMS script.
+    """
+
+    if not Path(working_dir).is_dir():
+        msg = f"Working directory {working_dir} does not exist."
+        raise FileNotFoundError(msg)
+    if not Path(params_path).is_file():
+        msg = f"Parameters file {params_path} does not exist."
+        raise FileNotFoundError(msg)
+    if not Path(coords_path).is_file():
+        msg = f"Coordinates file {coords_path} does not exist."
+        raise FileNotFoundError(msg)
+
+    process = subprocess.Popen(
+        [str(script_path)],
+        cwd=str(working_dir),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    if process.stdin is None:
+        msg = "Failed to open stdin for the process."
+        raise RuntimeError(msg)
+
+    time.sleep(0.01)
+    process.stdin.write(b"100\n")
+    process.stdin.flush()
+
+    time.sleep(0.01)
+    process.stdin.write(f"{params_path}\n".encode())
+    process.stdin.flush()
+
+    time.sleep(0.1)
+    process.stdin.write(b"y\n")  # Confirm to use custom parameters
+    process.stdin.flush()
+
+    time.sleep(0.01)
+    process.stdin.write(b"y\n")  # Want advanced options
+    process.stdin.flush()
+
+    time.sleep(0.01)
+    process.stdin.write(b"4\n")  # Select coordinates file
+    process.stdin.flush()
+
+    time.sleep(0.01)
+    process.stdin.write(f"{coords_path}\n".encode())
+    process.stdin.flush()
+
+    time.sleep(0.01)
+    process.stdin.write(b"0\n")  # No additional options
+    process.stdin.flush()
+
+    stdout, _ = process.communicate()
+    return stdout.decode()
